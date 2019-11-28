c677837416a3e61b625fd724db474a29d5b29a11
David <david.grande3049@ctag.com>
2019-11-28 16:20:53 +0100

    Clean Project

--- app/src/main/java/com/example/david/amigoinvisible/Helper.java
+++ app/src/main/java/com/example/david/amigoinvisible/Helper.java
@@ -2,14 +2,11 @@ package com.example.david.amigoinvisible;
 
 
 import android.content.Context;
-import android.database.sqlite.SQLiteDatabase;
 
 import com.readystatesoftware.sqliteasset.SQLiteAssetHelper;
 
 public class Helper extends SQLiteAssetHelper {
 
-   /* private static String DB_PATH = "/data/data/com.example.david.amigoinvisible/databases/";
-    private SQLiteDatabase myDataBase;*/
     private static final String DB_NAME = "AmigoInvisible.db";
     private static final int DB_VERSION = 1;
 
